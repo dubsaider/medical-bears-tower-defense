6305cdfeb058428329ebfbcab28914814cf83f86
Михаил Свинцицкий <starenginegames@gmail.com>
2024-12-04 00:49:41 +1000

    добавил область спавна

--- MedicalBears/Assets/NavMeshBaker.cs
+++ MedicalBears/Assets/NavMeshBaker.cs
@@ -1,19 +1,18 @@
+using Code.Core;
 using NavMeshPlus.Components;
 using UnityEngine;
 
 public class NavMeshBaker : MonoBehaviour
 {
-    public static NavMeshBaker Instance;
-    
     private NavMeshSurface Surface2D;
 
     private void Awake()
     {
-        Instance = this;
         Surface2D = GetComponent<NavMeshSurface>();
+        CoreEventsProvider.LevelStarted += Bake;
     }
 
-    public void Bake()
+    private void Bake()
     {
         Surface2D.BuildNavMeshAsync();
     }
