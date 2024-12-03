1cfa589420bbfb4b89ba9ce32cbe9d8ff8b2364c
Михаил Свинцицкий <starenginegames@gmail.com>
2024-12-03 20:08:03 +1000

    Доделал логику запуска новеллы

--- MedicalBears/Assets/Code/Entities/Level.cs
+++ MedicalBears/Assets/Code/Entities/Level.cs
@@ -5,6 +5,7 @@ namespace Code.Entities
     [CreateAssetMenu(menuName = "MedicalBears/Level")]
     public class Level : ScriptableObject
     {
+        public DialogueSession dialogueSession;
         public int mapIndex;
         public int startBalance;
         public Wave[] waves;
